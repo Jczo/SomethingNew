7c35ade6f900c95c3c8fd5d06a1ded3826850678
Jczo <67400793+Jczo@users.noreply.github.com>
2021-01-08 11:43:14 -0500

    Jan8pypractice

---
+++ jan8pypractice/script2.py
@@ -0,0 +1,5 @@
+#SLICING
+tvshows = ['star trek','got', 'stranger things','big bang theory','black mirror']
+
+print(tvshows[0:3])
+print(tvshows[:3])
---
+++ jan8pypractice/script3.py
@@ -0,0 +1,12 @@
+family = ['jose','angelica','candida','jaime','hugo','esperanza']
+
+print(family[:2])
+print(family[0:4])
+print(family[:1])
+print(family[1:3])
+print(family[4:5])
+print(family[:0])
+print(family[3:4])
+print(family[:5])
+print(family[2:3])
+print(family[1:2])
